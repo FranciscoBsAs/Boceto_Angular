fa73c84b56fa15eaa2eae3ebbbe3f8ad0af4c538
Francisco T.C <franciscotchiressi@gmail.com>
2025-07-19 19:58:57 -0300

    feature: added the initials steps and i Angular Material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Toolbar } from './toolbar/toolbar';
+import { Navbar } from './navbar/navbar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Toolbar, Navbar, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
