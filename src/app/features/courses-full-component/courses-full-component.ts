24a7974799ec733f24d28a69b4f2c57910a0e73f
Francisco T.C <franciscotchiressi@gmail.com>
2025-08-09 00:26:44 -0300

    feature: implement courses display and deletion functionality in courses-full-component, all synchronizing with course API service and courses-table

--- src/app/features/courses-full-component/courses-full-component.ts
+++ src/app/features/courses-full-component/courses-full-component.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+//import { StudentsTable } from '../students-full-component/students-table/students-table';
+import { Router, RouterModule } from '@angular/router';
+import { CoursesApiService } from './courses-api-service';
+import { courseInterface } from '../../../shared/sharedContent/entities';
+import { CoursesTable } from './courses-table/courses-table';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-courses-full-component',
-  imports: [],
+  imports: [CommonModule, CoursesTable, RouterModule],
   templateUrl: './courses-full-component.html',
   styleUrl: './courses-full-component.css'
 })
-export class CoursesFullComponent {
+
+export class CoursesFullComponent implements OnInit {
+
+  constructor(
+
+    private courseAPI : CoursesApiService,
+    private theRouter : Router
+
+  ){}
+
+  coursesArray! : courseInterface[]
+
+  
+  ngOnInit() : void {
+      
+    this.courseAPI.getCoursesThroughMockIO().subscribe( ( coursesFromDB ) => {      // coursesFromDB infiere desde el generic <courseInterface[]> su type de parametro callback a esperar
+
+      console.table( coursesFromDB ) ;
+
+      this.coursesArray = coursesFromDB
+
+    } )
+
+  }
+
+
+  handleDeleteCourse ( courseToDelete : courseInterface ) : void {
+
+    console.log( "Eliminando curso", courseToDelete ) ; console.table( courseToDelete )
+
+    this.courseAPI.deleteCourseInDB( courseToDelete ).pipe(
+
+      switchMap( () => this.courseAPI.getCoursesThroughMockIO() )
+
+    ).subscribe( ( updatedCoursesArray ) => {
+
+      this.coursesArray = updatedCoursesArray
+
+    } )
+
+  }
 
 }
