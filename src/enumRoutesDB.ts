554da140ae152a299ee23caea35d7e390a278d6b
Francisco T.C <franciscotchiressi@gmail.com>
2025-08-08 22:54:13 -0300

    feature: updated routing paths for consistency, such as ednpointURL to the MockDB and view & edit routing

--- src/enumRoutesDB.ts
+++ src/enumRoutesDB.ts
@@ -1,5 +1,7 @@
 export enum RoutingDB {
 
+    EDPOINT_APIio_DB = 'https://68916597447ff4f11fbc72b6.mockapi.io',
+
     STUDENTS = 'students',
     COURSES = 'courses'
 
--- src/shared/urlRoutesEnum.ts
+++ src/shared/urlRoutesEnum.ts
@@ -1,11 +1,15 @@
 export enum RoutingPaths {
 
-    HOME = 'home',
+    HOME = '',
     USERS = 'usuarios',
     STUDENTS = 'estudiantes',
     COURSES = 'cursos',
-    REGISTRATIONS= 'inscripciones', //registrations
-    VIEW_SINGULAR_STUDENT = 'view-student',
-    EDIT_SINGULAR_STUDENT = 'edit-student'
+    REGISTRATIONS= 'inscripciones',
+    
+    VIEW_SINGULAR_STUDENT = 'ver-estudiante',
+    EDIT_SINGULAR_STUDENT = 'editar-estudiante',
+
+    EDIT_SINGULAR_COURSE = 'editar-curso',
+    VIEW_SINGULAR_COURSE = 'ver-curso'
    
 }
