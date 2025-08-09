906b764c1aac8b73728ab8254ca5c3be0a787d2e
Francisco T.C <franciscotchiressi@gmail.com>
2025-08-09 00:32:25 -0300

    feature: reorganize routing paths for students, courses and not-found comp, ensuring proper component lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,12 @@
 import { Routes } from '@angular/router';
-import { Home } from './home/home';
 import { RoutingPaths } from '../shared/urlRoutesEnum';
 import { StudentsFullComponent } from './features/students-full-component/students-full-component';
-import { ViewSingularStudent } from './features/students-full-component/view-singular-student/view-singular-student';
-import { EditForm } from './edit-form/edit-form';
+
 
 export const routes : Routes = [
 
     {
-        path: '',
+        path: RoutingPaths.HOME,
         component: StudentsFullComponent,
         /*
 
@@ -27,10 +25,7 @@ export const routes : Routes = [
             )
         }
     },
-    {
-        path: RoutingPaths.COURSES ,
-        loadComponent: () => import( './features/courses-full-component/courses-full-component').then( ( module ) => module.CoursesFullComponent )
-    },
+
     {
         path: RoutingPaths.REGISTRATIONS,
         loadComponent: () => import( './features/registrations-full-component/registrations-full-component' ).then( (module) => module.RegistrationsFullComponent )
@@ -42,7 +37,28 @@ export const routes : Routes = [
     },
     {
         path: RoutingPaths.EDIT_SINGULAR_STUDENT,
-        loadComponent: () => import( './edit-form/edit-form' ).then( (module) => module.EditForm )
-    }
+        //loadComponent: () => import( './edit-form/edit-student-form' ).then( (module) => module.EditStudentForm )
+        loadComponent: () => import( './features/students-full-component/edit-student-form/edit-student-form' ).then( (module) => module.EditStudentForm )
+    },
 
+    {
+        path: RoutingPaths.COURSES ,
+        loadComponent: () => import( './features/courses-full-component/courses-full-component').then( ( module ) => module.CoursesFullComponent )
+    },
+    {
+        path: RoutingPaths.EDIT_SINGULAR_COURSE,
+        loadComponent: () => import( './features/courses-full-component/edit-courses-form/edit-courses-form' ).then( (module) => module.EditCoursesForm )
+    },
+    {
+        path: RoutingPaths.VIEW_SINGULAR_COURSE,
+        loadComponent: () => import( './features/courses-full-component/view-singular-course/view-singular-course' ).then( ( module ) => module.ViewSingularCourse )
+    },
+
+    
+
+    {
+        path: "**",
+        //component: NotFoundComponent
+        loadComponent: () => import( './features/not-found-component/not-found-component' ).then( ( module ) => module.NotFoundComponent )
+    }
 ];
