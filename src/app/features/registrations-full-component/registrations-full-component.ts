693d6fa9701929d041508c621bb3ee569566ed2f
Francisco T.C <franciscotchiressi@gmail.com>
2025-08-15 16:14:31 -0300

    feature: more Preparations registrationsComponent

--- src/app/features/registrations-full-component/registrations-full-component.ts
+++ src/app/features/registrations-full-component/registrations-full-component.ts
@@ -1,14 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MyMatCommonRouterModule } from '../my-mat-common-router/my-mat-common-router-module';
-import { studentInterface } from '../../../shared/sharedContent/entities';
-import { StudentsAPIService } from '../students-full-component/students-api-service';
-import { averageSup0, firstLetterUpperCaseValidator, onlyLettersValidator } from '../../../shared/validatorFunctions/ValidatorFunctions';
-import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-registrations-full-component',
-  imports: [ MyMatCommonRouterModule, ReactiveFormsModule ],
+  imports: [ MyMatCommonRouterModule ],
   templateUrl: './registrations-full-component.html',
   styleUrl: './registrations-full-component.css'
 })
